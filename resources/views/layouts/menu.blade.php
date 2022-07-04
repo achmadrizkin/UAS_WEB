01906f78335a18a16d55a26877a3f9463555d503
achmadrizkin <arizki.nf02@gmail.com>
2022-07-04 18:12:36 +0700

    update layout

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,9 +2,6 @@
 <div id="menu">
 <ul id="list-menu">
     <li><a href="{{route('homepage')}}">Home</a></li>
-    <li><a href="{{route('produk')}}">Produk</a></li>
-    <li><a href="{{route('profil')}}">Profil</a></li>
-    <li><a href="">Kontak</a></li>
     @if(Auth::check())
         <li><a href="{{route('kategori.index')}}">Merek</a></li>
         <li><a href="{{route('posting.index')}}">Mobil</a></li>
